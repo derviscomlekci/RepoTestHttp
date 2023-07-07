58d8dd84b8f1b01758634c3a06e4f508633c8ca5
Hydofbl <hidayetfurkanbirel@gmail.com>
2023-07-07 12:04:13 +0300

    Server files readded.

---
+++ Server/Business/Abstract/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IDataResult<User>> GetCount();
+
+        Task<IResult> Add(User user);
+
+        Task<IResult> UpdateCount(User user);
+    }
+}
---
+++ Server/Business/Concreate/UserManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Core.Utilities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concreate
+{
+    public class UserManager:IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public async Task<IResult>  Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccesResult("Added");
+        }
+
+        public async Task<IDataResult<User>> GetCount()
+        {
+            var result =await _userDal.GetAll();
+            return new SuccessDataResult<User>(result.First());
+        }
+
+        public async Task<IResult> UpdateCount(User user)
+        {
+            await _userDal.Update(user);
+            return new SuccesResult("updated");
+        }
+    }
+}
